72ca7e1756bbf4e290ada609eb725d68854ed298
Indah Trivena <indahtrivena124@email.com>
2021-02-07 23:12:41 +0700

    7-feb-2021

---
+++ 1006 AVERAGE 2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    float A, B, C, MEDIA;
+	
+	cin>>A>>B>>C;
+	
+	MEDIA = ((A*2)+(B*3)+(C*5))/10;
+	
+	printf("MEDIA = %.1f\n", MEDIA);
+ 
+    return 0;
+}
