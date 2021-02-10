f2257c1df91e334de78acdb7d2644dcd063d8e0c
Indah Trivena <indahtrivena124@email.com>
2021-02-10 22:54:07 +0700

    10-feb-2021

---
+++ 1011 Sphere.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int R;
+    double VOLUME;
+    double phi = 3.14159;
+ 
+ 	scanf("%d", &R);
+ 	
+	VOLUME = (4 * phi * pow(R, 3))/3;
+ 	
+	printf("VOLUME = %.3lf\n", VOLUME);
+	
+ 
+    return 0;
+}
